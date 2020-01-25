3ce9c06724ad2b2dce048cef4c5e61ee15428d73
jgaffuri <jgaf@protonmail.com>
2020-01-25 11:02:13 +0100

    new operations

--- src/main/java/eu/europa/ec/eurostat/java4eurostat/analysis/Operations.java
+++ src/main/java/eu/europa/ec/eurostat/java4eurostat/analysis/Operations.java
@@ -15,49 +15,155 @@ import eu.europa.ec.eurostat.java4eurostat.base.StatsIndex;
  */
 public class Operations {
 
-	public static StatsHypercube computeDifference(StatsHypercube hc1, StatsHypercube hc2){ return computeDifference(hc1, hc2, false, false); }
 	/**
-	 * Compute difference of two stat datasets. It is assumed both have the same structure.
+	 * A unary operation
 	 * 
-	 * @param hc1 The first stat dataset
-	 * @param hc2 The second stat dataset
-	 * @param abs Compute absolute value of the difference
-	 * @param ratio Compute percentage difference
+	 * @author julien Gaffuri
+	 *
+	 */
+	public interface UnaryOperation {
+		double compute(double val);
+	}
+
+	/**
+	 * Compute a unary operation.
+	 * 
+	 * @param hc
+	 * @param uop
 	 * @return
 	 */
-	public static StatsHypercube computeDifference(StatsHypercube hc1, StatsHypercube hc2, boolean abs, boolean ratio){
-		//TODO simplify it - extract functions
-
-		String[] dimLabels = hc1.getDimLabels();
+	public static StatsHypercube compute(StatsHypercube hc, UnaryOperation uop){
+		String[] dimLabels = hc.getDimLabels();
 		StatsHypercube out = new StatsHypercube(dimLabels);
-		StatsIndex hcI2 = new StatsIndex(hc2, dimLabels);
-		for(Stat s : hc1.stats){
+		for(Stat s : hc.stats){
 			//get stat value
 			double val = s.value;
 			if(Double.isNaN(val)) continue;
 
-			//retrieve value to compare with
-			String[] dimValues = new String[dimLabels.length];
-			for(int i=0; i<dimLabels.length; i++) dimValues[i] = s.dims.get(dimLabels[i]);
-			double valVal = hcI2.getSingleValue(dimValues);
-			if(Double.isNaN(valVal)) continue;
-
 			//compute comparison figure
-			double compVal = valVal - val;
-			if(ratio) compVal*=100/val;
-			if(abs) compVal=Math.abs(compVal);
+			double outVal = uop.compute(val);
+			if(Double.isNaN(val)) continue;
 
 			//store comparison figures
-			Stat sc = new Stat(compVal);
+			Stat sc = new Stat(outVal);
 			for(int i=0; i<dimLabels.length; i++) sc.dims.put(dimLabels[i], s.dims.get(dimLabels[i]));
 			out.stats.add(sc);
 		}
 		return out;
 	}
 
-	
-	//TODO do sum
-	//TODO do division
-	//TODO do multi
-	
+	/**
+	 * A binary operation
+	 * 
+	 * @author julien Gaffuri
+	 *
+	 */
+	public interface BinaryOperation {
+		double compute(double val1, double val2);
+	}
+
+	/**
+	 * Compute a binary operation.
+	 * 
+	 * @param hc1
+	 * @param hc2
+	 * @param bop
+	 * @return
+	 */
+	public static StatsHypercube compute(StatsHypercube hc1, StatsHypercube hc2, BinaryOperation bop){
+		String[] dimLabels = hc1.getDimLabels();
+		StatsHypercube out = new StatsHypercube(dimLabels);
+		StatsIndex hcI2 = new StatsIndex(hc2, dimLabels);
+		for(Stat s : hc1.stats){
+			//get stat value
+			double val1 = s.value;
+			if(Double.isNaN(val1)) continue;
+
+			//retrieve value to compare with
+			String[] dimValues = new String[dimLabels.length];
+			for(int i=0; i<dimLabels.length; i++) dimValues[i] = s.dims.get(dimLabels[i]);
+			double val2 = hcI2.getSingleValue(dimValues);
+			if(Double.isNaN(val2)) continue;
+
+			//compute comparison figure
+			double outVal = bop.compute(val1, val2);
+			if(Double.isNaN(outVal)) continue;
+
+			//store comparison figures
+			Stat sc = new Stat(outVal);
+			for(int i=0; i<dimLabels.length; i++) sc.dims.put(dimLabels[i], s.dims.get(dimLabels[i]));
+			out.stats.add(sc);
+		}
+		return out;
+	}
+
+
+
+
+
+	public static StatsHypercube opp(StatsHypercube hc){
+		return compute(hc, new UnaryOperation() {
+			@Override
+			public double compute(double val) {
+				return -val;
+			}});
+	}
+	public static StatsHypercube abs(StatsHypercube hc){
+		return compute(hc, new UnaryOperation() {
+			@Override
+			public double compute(double val) {
+				return Math.abs(val);
+			}});
+	}
+	public static StatsHypercube sum(StatsHypercube hc, double valueToSum){
+		return compute(hc, new UnaryOperation() {
+			@Override
+			public double compute(double val) {
+				return val + valueToSum;
+			}});
+	}
+
+	public static StatsHypercube sum(StatsHypercube hc1, StatsHypercube hc2){
+		return compute(hc1, hc2, new BinaryOperation() {
+			@Override
+			public double compute(double val1, double val2) {
+				return val1+val2;
+			}});
+	}
+	public static StatsHypercube diff(StatsHypercube hc1, StatsHypercube hc2){
+		return compute(hc1, hc2, new BinaryOperation() {
+			@Override
+			public double compute(double val1, double val2) {
+				return val1-val2;
+			}});
+	}
+	public static StatsHypercube div(StatsHypercube hc1, StatsHypercube hc2){
+		return compute(hc1, hc2, new BinaryOperation() {
+			@Override
+			public double compute(double val1, double val2) {
+				return val1/val2;
+			}});
+	}
+	public static StatsHypercube mult(StatsHypercube hc1, StatsHypercube hc2){
+		return compute(hc1, hc2, new BinaryOperation() {
+			@Override
+			public double compute(double val1, double val2) {
+				return val1*val2;
+			}});
+	}
+
+	/**
+	 * Compute the total of all values along a dimension
+	 * 
+	 * @param hc
+	 * @param dim
+	 * @param totalDimValue
+	 */
+	public void computeTotalDim(StatsHypercube hc, String dim, String totalDimValue) {
+		//TODO index along the other dimensions
+		//TODO go accros all other dimensions
+		//TODO compute total
+		//TODO create new stat with total value
+	}
+
 }
