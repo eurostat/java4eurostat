f3e013b6c9dfda2959fa90a1f158ec1292ff9880
jgaffuri <jgaf@protonmail.com>
2020-01-25 11:50:08 +0100

    compacity

---
+++ src/main/java/eu/europa/ec/eurostat/java4eurostat/analysis/Compacity.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package eu.europa.ec.eurostat.java4eurostat.analysis;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class Compacity {
+
+	//TODO check compacity
+	//number of stat values provided / number of spaces
+
+	//TODO make compact
+	//by adding missing values
+
+}
