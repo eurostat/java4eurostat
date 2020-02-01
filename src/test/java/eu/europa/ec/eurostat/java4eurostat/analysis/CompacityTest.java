d97b62d24a419eb9d4d5784ff494069381e2bc73
jgaffuri <jgaf@protonmail.com>
2020-02-01 10:36:01 +0100

    new test

---
+++ src/test/java/eu/europa/ec/eurostat/java4eurostat/analysis/CompacityTest.java
@@ -0,0 +1,19 @@
+package eu.europa.ec.eurostat.java4eurostat.analysis;
+
+import junit.framework.TestCase;
+
+public class CompacityTest extends TestCase {
+
+	/*public static void main(String[] args) throws Exception {
+		junit.textui.TestRunner.run(CompacityTest.class);
+	}*/
+
+
+	public void test1() throws Exception {
+
+		//assertEquals(14, fsFin.size());
+
+
+	}
+
+}
