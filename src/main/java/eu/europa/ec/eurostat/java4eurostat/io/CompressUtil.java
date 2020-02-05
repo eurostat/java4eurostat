3d10d8bbd7103b95c699545db55766fe3a7fb7c5
jgaffuri <jgaf@protonmail.com>
2020-02-05 08:57:06 +0100

    new unzip function

--- src/main/java/eu/europa/ec/eurostat/java4eurostat/io/CompressUtil.java
+++ src/main/java/eu/europa/ec/eurostat/java4eurostat/io/CompressUtil.java
@@ -6,10 +6,12 @@ package eu.europa.ec.eurostat.java4eurostat.io;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -48,7 +50,10 @@ public class CompressUtil {
 			if(outFile.exists()) outFile.delete();
 			ZipOutputStream out = new ZipOutputStream(new FileOutputStream(outFile));
 			for(String inFile : inFiles) {
-				if(!new File(inPath+inFile).exists()) continue;
+				if(!new File(inPath+inFile).exists()) {
+					System.err.println("Could not add file "+inPath+inFile+" to zip: Not found.");
+					continue;
+				}
 				ZipEntry e = new ZipEntry(inFile);
 				out.putNextEntry(e);
 				byte[] data = Files.readAllBytes(Paths.get(inPath+inFile));
@@ -58,6 +63,38 @@ public class CompressUtil {
 			out.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+
+
+	/**
+	 * @param zipFile
+	 * @param outputFolder
+	 */
+	public static void unZip(String zipFile, String outputFolder){
+		try{
+			//output directory
+			File folder = new File(outputFolder);
+			if(!folder.exists()) folder.mkdir();
+
+			ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+			ZipEntry ze = zis.getNextEntry();
+			byte[] buffer = new byte[1024];
+			while(ze!=null){
+				String fileName = ze.getName();
+				File newFile = new File(outputFolder + File.separator + fileName);
+				new File(newFile.getParent()).mkdirs();
+				FileOutputStream fos = new FileOutputStream(newFile);
+				int len;
+				while ((len = zis.read(buffer)) > 0) fos.write(buffer, 0, len);
+				fos.close();
+				ze = zis.getNextEntry();
+			}
+			zis.closeEntry();
+			zis.close();
+		}catch(IOException ex){
+			ex.printStackTrace();
 		}
 	}
 
